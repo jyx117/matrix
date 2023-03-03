1d1c20b0cac0c5ed9ff60bc3b936e0f8c5872d44
michael <michaeljyx117@gmail.com>
2023-03-03 17:46:27 +0800

    增加拦截器定义

---
+++ start/src/main/java/com/cloud/matrix/start/interceptor/LoginInterceptor.java
@@ -0,0 +1,9 @@
+package com.cloud.matrix.start.interceptor;
+
+/**
+ * 用户登录拦截器
+ * @author michael
+ * @version $Id: LoginInterceptor.java, v 0.1 2023-03-03 5:44 PM Michael Exp $$
+ */
+public class LoginInterceptor {
+}
---
+++ start/src/main/java/com/cloud/matrix/start/interceptor/TenantInterceptor.java
@@ -0,0 +1,9 @@
+package com.cloud.matrix.start.interceptor;
+
+/**
+ * 租户拦截器
+ * @author michael
+ * @version $Id: TenantInterceptor.java, v 0.1 2023-03-03 5:44 PM Michael Exp $$
+ */
+public class TenantInterceptor {
+}
